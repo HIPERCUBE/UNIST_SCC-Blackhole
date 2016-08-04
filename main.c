18efb3c2d8c1d2261d92ad97d34a7cb6e877e6d7
Seungwoo Yeom <ysw0094@gmail.com>
2016-08-04 09:41:05 +0900

    Enable Multi Core Parallelization with MPI

--- main.c
+++ main.c
@@ -18,13 +18,9 @@
 #define C_EXPORT_IMAGE_HEIGHT       100          // it should be a multiple of num_procs.
 #define C_EXPORT_IMAGE_TOTAL_PIXELS (C_EXPORT_IMAGE_WIDTH * C_EXPORT_IMAGE_HEIGHT)
 
-// Information for job division
-#define C_NUMBER_OF_DIVISION        4
+void gather_results(RESULT *result, int number_of_results, RESULT *collection);
 
-//void gather_results(RESULT *result, int number_of_results, RESULT *collection);
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     BLACKHOLE_LAB *blackhole_lab = NULL;
     RESULT *result = NULL;
     int number_of_results;
@@ -34,67 +30,67 @@ int main(int argc, char **argv)
     int my_rank, num_procs;
     int i, j;
 
-    collection = (RESULT*)calloc(C_EXPORT_IMAGE_TOTAL_PIXELS, sizeof(RESULT));
+    collection = (RESULT *) calloc(C_EXPORT_IMAGE_TOTAL_PIXELS, sizeof(RESULT));
 
-    num_procs = C_NUMBER_OF_DIVISION;
+    MPI_Init(&argc, argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
     x_start = 0;
-    x_end   = C_EXPORT_IMAGE_WIDTH - 1;
+    x_end = C_EXPORT_IMAGE_WIDTH - 1;
 
-    for (my_rank = 0, i = 0; my_rank < num_procs; my_rank++) {
-        y_start = (C_EXPORT_IMAGE_HEIGHT / num_procs) * my_rank;
-        y_end = (C_EXPORT_IMAGE_HEIGHT / num_procs) * (my_rank + 1) - 1;
+    y_start = (C_EXPORT_IMAGE_HEIGHT / num_procs) * my_rank;
+    y_end = (C_EXPORT_IMAGE_HEIGHT / num_procs) * (my_rank + 1) - 1;
 
-        blackhole_lab = BLACKHOLE_LAB_create(
+    blackhole_lab = BLACKHOLE_LAB_create(
             C_EXPORT_IMAGE_WIDTH, x_start, x_end,
             C_EXPORT_IMAGE_HEIGHT, y_start, y_end,
             C_VISUAL_ANGLE, C_TIME_STEP, C_TIME_INTERVAL
-        );
+    );
 
-        if (NULL == blackhole_lab) {
-            BLACKHOLE_LAB_destroy(blackhole_lab);
-            fprintf(stderr, "Creating BLACKHOLE_LAB is Failed.\n");
-            return -1;
-        }
-
-        BLACKHOLE_LAB_start_experiment(blackhole_lab);
-
-        number_of_results = C_EXPORT_IMAGE_WIDTH * C_EXPORT_IMAGE_HEIGHT / num_procs;
-
-        result = (RESULT*)calloc(number_of_results, sizeof(RESULT));
-
-        BLACKHOLE_LAB_dump_results(blackhole_lab, result);
-
+    if (NULL == blackhole_lab) {
         BLACKHOLE_LAB_destroy(blackhole_lab);
-
-        for (j = 0; j < number_of_results; j++, i++) {
-            collection[i] = result[j];
-        }
-
-        free(result);
+        fprintf(stderr, "Creating BLACKHOLE_LAB is Failed.\n");
+        return -1;
     }
 
-    BLACKHOLE_LAB_make_vision(collection, C_IMPORT_IMAGE_FILENAME, C_EXPORT_IMAGE_FILENAME, C_EXPORT_IMAGE_WIDTH, C_EXPORT_IMAGE_HEIGHT);
+    BLACKHOLE_LAB_start_experiment(blackhole_lab);
+
+    number_of_results = C_EXPORT_IMAGE_WIDTH * C_EXPORT_IMAGE_HEIGHT / num_procs;
+
+    result = (RESULT *) calloc(number_of_results, sizeof(RESULT));
+
+    BLACKHOLE_LAB_dump_results(blackhole_lab, result);
+
+    BLACKHOLE_LAB_destroy(blackhole_lab);
+
+    free(result);
+
+    if (my_rank == 1) {
+        BLACKHOLE_LAB_make_vision(collection, C_IMPORT_IMAGE_FILENAME, C_EXPORT_IMAGE_FILENAME, C_EXPORT_IMAGE_WIDTH,
+                                  C_EXPORT_IMAGE_HEIGHT);
+    }
 
     free(collection);
+
+    MPI_Finalize();
 
     return 0;
 }
 
-/*
- void gather_results(RESULT *result, int number_of_results, RESULT *collection)
- {
-     int i;
-     MPI_Datatype mpi_type;
-     int member_block_length[4] = {1, 1, 1, 1};
-     MPI_Datatype member_type[4] = {MPI_INT, MPI_DOUBLE, MPI_DOUBLE, MPI_INT};
-     MPI_Aint member_displacement[4] = {offsetof(RESULT, status), offsetof(RESULT, x), offsetof(RESULT, y), offsetof(RESULT, pixel_index)};
- 
-     MPI_Type_struct(4, member_block_length, member_displacement, member_type, &mpi_type);
-     MPI_Type_commit(&mpi_type);
-     
-     MPI_Gather(result, number_of_results, mpi_type, collection, number_of_results, mpi_type, 0, MPI_COMM_WORLD);
-     
-     MPI_Type_free(&mpi_type);
- }
- */
+void gather_results(RESULT *result, int number_of_results, RESULT *collection) {
+    int i;
+    MPI_Datatype mpi_type;
+    int member_block_length[4] = {1, 1, 1, 1};
+    MPI_Datatype member_type[4] = {MPI_INT, MPI_DOUBLE, MPI_DOUBLE, MPI_INT};
+    MPI_Aint member_displacement[4] = {offsetof(RESULT, status), offsetof(RESULT, x), offsetof(RESULT, y),
+                                       offsetof(RESULT, pixel_index)};
+
+    MPI_Type_struct(4, member_block_length, member_displacement, member_type, &mpi_type);
+    MPI_Type_commit(&mpi_type);
+
+    MPI_Gather(result, number_of_results, mpi_type, collection, number_of_results, mpi_type, 0, MPI_COMM_WORLD);
+
+    MPI_Type_free(&mpi_type);
+}
